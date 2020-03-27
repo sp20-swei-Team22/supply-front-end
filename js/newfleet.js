474fc1e5cb9c8d46282d2d6ea1d7a1a08f8035a5
Jeffrey Ng <komoto415@gmail.com>
2020-03-26 20:59:05 -0500

    ready to test adding fleet to db

---
+++ js/newfleet.js
@@ -0,0 +1,34 @@
+$('#newFleetForm').submit(async e => {
+    e.preventDefault();
+    
+    var url = 'https://supply.team22.softwareengineeringii.com/addFleet';
+    
+    var fleet = {};
+    form = document.getElementById(this.id);
+    fields = form.querySelectorAll('input, select');
+    // console.log(form);
+    console.log(fields);
+    
+    fields.forEach((e, i) => {
+        // console.log(e.id);
+        fleet[`${e.id}`] = e.value;
+    });
+    // console.log(fleet);
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        mode: 'no-cors',
+        body: JSON.stringify(fleet)
+    }).then(res => {
+        if (res.status == 200) {
+            alert('Fleet added!');
+        } else {
+            alert('Something went wrong');
+        }
+    }).catch(err => {
+        console.log('Error: ', err);
+    });
+})
