9c8eb1dc46d7b49f05f67c68d81b09bfc14fe245
Jeffrey Ng <komoto415@gmail.com>
2020-03-26 20:59:37 -0500

    unique fleet tab divs

---
+++ js/fleetvisualisationswitcher.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('input[type=radio]').click(function() {
+        const radId = $(this).attr('id');
+        // console.log(radId);
+        let endpoint = radId.includes('Map') ? radId.indexOf('Map') : radId.indexOf('Table');
+        let idHeader = '#' + radId.substring(0, endpoint);
+        let mapDiv = idHeader + 'MapDiv'
+        let tableDiv = idHeader + 'TableDiv'
+        // console.log(mapDiv);
+        // console.log(tableDiv)
+        if($(this).attr('value') == 'map') {
+            $(mapDiv).show()
+            $(tableDiv).hide()
+        } else {
+            $(mapDiv).hide()
+            $(tableDiv).show()
+        }
+    });
+});
