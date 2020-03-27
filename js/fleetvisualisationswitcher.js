c4a3fa7ff3849e4cc1db9a94ac4f9a87d64b11ba
Jeffrey Ng <komoto415@gmail.com>
2020-03-27 15:58:07 -0500

    uses .on instead of .click for dynamic inputs

--- js/fleetvisualisationswitcher.js
+++ js/fleetvisualisationswitcher.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
-    $('input[type=radio]').click(function() {
-        const radId = $(this).attr('id');
-        console.log(radId);
-        let endpoint = radId.includes('Map') ? radId.indexOf('Map') : radId.indexOf('Table');
-        let idHeader = '#' + radId.substring(0, endpoint);
-        let mapDiv = idHeader + 'MapDiv'
-        let tableDiv = idHeader + 'TableDiv'
-        // console.log(mapDiv);
-        // console.log(tableDiv)
-        if($(this).attr('id').includes('Map')) {
-            $(mapDiv).show()
-            $(tableDiv).hide()
-        } else {
-            $(mapDiv).hide()
-            $(tableDiv).show()
-        }
-    });
+$(document).on('click', 'input[type=radio]', (e) => {
+    const radio = e.target
+    // console.log(radio);
+    const radId = radio.id;
+    // console.log(radId);
+    let endpoint = radId.includes('Map') ? radId.indexOf('Map') : radId.indexOf('Table');
+    let idHeader = '#' + radId.substring(0, endpoint);
+    let mapDiv = idHeader + 'MapDiv'
+    let tableDiv = idHeader + 'TableDiv'
+    // console.log(mapDiv);
+    // console.log(tableDiv)
+    if (radId.includes('Map')) {
+        $(mapDiv).show()
+        $(tableDiv).hide()
+    } else {
+        $(mapDiv).hide()
+        $(tableDiv).show()
+    }
 });
