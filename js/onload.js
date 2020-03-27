63776759db6cad4eea264b5df95f4c48c3a0d4d3
Jeffrey Ng <komoto415@gmail.com>
2020-03-27 16:35:36 -0500

    space formatting

--- js/onload.js
+++ js/onload.js
@@ -3,12 +3,12 @@ let loadTables = () => {
     let identity = localStorage.getItem('username');
     // console.log(identity)
     document.getElementById('accountName').text = identity;
-    
+
     var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
         params = {
             'user': identity
         }
-    
+
     if (isEmail(identity)) {
         let arr = identity.split('@');
         // console.log(arr);
@@ -18,10 +18,10 @@ let loadTables = () => {
                 'emailExt': arr[1],
             }
     }
-    
+
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
     // console.log(url)
-    
+
     fetch(url).then(res => {
         res.json().then(json => {
             if (res.status == 200) {
@@ -39,7 +39,7 @@ let loadTables = () => {
                     );
                 })
                 // console.log(arr);
-    
+
                 fleets = {}
                 arr.forEach(function (e) {
                     key = e[0];
@@ -50,12 +50,12 @@ let loadTables = () => {
                     fleets[key].push(e);
                 });
                 // console.log(fleets);
-    
+
                 const colNames = ['Vehicle ID', 'Status', 'Vehicle Type', 'Date Added', 'Liscence Plate', 'Last Heartbeat'];
-    
+
                 var myTab = document.getElementById('myTab');
                 var myTabContent = document.getElementById('myTabContent');
-    
+
                 Object.keys(fleets).forEach(fleetNum => {
                     let idHeader = `fleet${fleetNum}`;
                     /*
@@ -63,7 +63,7 @@ let loadTables = () => {
                     */
                     let tabLI = document.createElement('LI');
                     tabLI.setAttribute('class', 'nav-item');
-    
+
                     let tabA = document.createElement('A');
                     let tabId = `${idHeader}Tab`
                     tabA.setAttribute('class', 'nav-link')
@@ -74,10 +74,10 @@ let loadTables = () => {
                     tabA.setAttribute('aria-controls', idHeader);
                     tabA.setAttribute('aria-selected', 'false');
                     tabA.innerHTML = `Fleet ${fleetNum}`;
-    
+
                     tabLI.appendChild(tabA);
                     myTab.insertBefore(tabLI, myTab.children[1]);
-    
+
                     /* 
                         Now the tab content
                     */
@@ -86,54 +86,54 @@ let loadTables = () => {
                     tabContentContainer.setAttribute('id', idHeader);
                     tabContentContainer.setAttribute('role', 'tabpanel');
                     tabContentContainer.setAttribute('aria-labelledby', `${idHeader}Tab`)
-    
+
                     let radioForm = document.createElement('FORM');
-    
+
                     let row = document.createElement('DIV');
                     row.setAttribute('class', 'row justify-content-center');
-    
+
                     ['Map', 'Table'].forEach(e => {
                         radDiv = document.createElement('DIV');
                         e == 'Map' ?
                             radDiv.setAttribute('style', 'margin-right: 5vw;') :
                             radDiv.setAttribute('style', 'margin-left: 5vw;');
-    
+
                         let radio = document.createElement('INPUT');
                         radio.setAttribute('type', 'radio');
                         radio.setAttribute('name', 'vis');
                         radio.setAttribute('id', `${idHeader}${e}Rad`)
                         if (e == 'Map') radio.setAttribute('checked', 'true');
-    
+
                         let label = document.createElement('LABEL');
                         label.setAttribute('for', `#${idHeader}${e}Rad`);
                         label.innerHTML = e;
-    
+
                         radDiv.appendChild(radio);
                         radDiv.appendChild(label);
                         row.appendChild(radDiv);
                     })
-    
+
                     radioForm.appendChild(row);
                     tabContentContainer.appendChild(radioForm);
-    
+
                     let mapDiv = document.createElement('DIV');
                     mapDiv.setAttribute('id', `${idHeader}MapDiv`);
                     mapDiv.setAttribute('class', 'mapDiv');
                     mapDiv.innerHTML = 'Map goes here';
-    
+
                     tabContentContainer.appendChild(mapDiv);
-    
+
                     let tableDiv = document.createElement('DIV');
                     tableDiv.setAttribute('id', `${idHeader}TableDiv`);
                     tableDiv.setAttribute('class', 'tableDiv');
                     fleetData = fleets[fleetNum];
                     // console.log(fleetData);
-    
+
                     let table = document.createElement('TABLE');
                     table.setAttribute('id', `${idHeader}Table`);
                     table.setAttribute('class', 'index table-bordered table-sm');
                     table.setAttribute('max-width', '100vw');
-    
+
                     let header = table.createTHead();
                     header.insertRow(0);
                     colNames.forEach(function (name) {
@@ -144,7 +144,7 @@ let loadTables = () => {
                     });
                     let tbody = document.createElement('TBODY');
                     table.appendChild(tbody);
-    
+
                     fleetData.forEach(entry => {
                         let row = document.createElement('TR');
                         entry.forEach((colVal, col, _) => {
@@ -160,7 +160,7 @@ let loadTables = () => {
                         });
                         tbody.appendChild(row);
                     });
-    
+
                     tableDiv.appendChild(table);
                     tabContentContainer.appendChild(tableDiv);
                     myTabContent.insertBefore(tabContentContainer, myTabContent.children[1]);
--- js/onloadLocal.js
+++ js/onloadLocal.js
@@ -3,29 +3,29 @@ let loadTable = () => {
     let identity = localStorage.getItem('username');
     // console.log(identity)
     document.getElementById('accountName').text = identity;
-    
+
     var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
         params = {
             'user': identity
         }
-    
+
     if (isEmail(identity)) {
         let arr = identity.split('@');
-        console.log(arr);
+        // console.log(arr);
         url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
             params = {
                 'user': arr[0],
                 'emailExt': arr[1],
             }
     }
-    
+
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    console.log(url)
-    
+    // console.log(url)
+
     fetch(url).then(res => {
         res.json().then(json => {
             if (res.status == 200) {
-                console.log(json);
+                // console.log(json);
                 alert('got the vehicles! if there are any ._.');
             } else {
                 alert('something went wrong');
@@ -34,7 +34,7 @@ let loadTable = () => {
     }).catch(err => {
         console.log('Error: ', err);
     });
-    
+
     json = [
         {
             'vehicleid': 1,
@@ -97,20 +97,20 @@ let loadTable = () => {
             'date_added': 5,
         }
     ];
-    
-    // console.log(json);
-    
+
     arr = []
     json.forEach(vehicleDict => {
         // console.log(vehicleDict);
+        let dateAdded = vehicleDict['date_added']
+        let trimmedDate = dateAdded.substring(0, dateAdded.indexOf('T'));
         arr.push(
             [vehicleDict['fleetid'], vehicleDict['vehicleid'], vehicleDict['status'],
-            `${vehicleDict['make']}: ${vehicleDict['model']}`, vehicleDict['date_added'],
+            `${vehicleDict['make']}: ${vehicleDict['model']}`, trimmedDate,
             vehicleDict['licenseplate'], vehicleDict['last_heartbeat']]
         );
     })
     // console.log(arr);
-    
+
     fleets = {}
     arr.forEach(function (e) {
         key = e[0];
@@ -121,12 +121,12 @@ let loadTable = () => {
         fleets[key].push(e);
     });
     // console.log(fleets);
-    
+
     const colNames = ['Vehicle ID', 'Status', 'Vehicle Type', 'Date Added', 'Liscence Plate', 'Last Heartbeat'];
-    
+
     var myTab = document.getElementById('myTab');
     var myTabContent = document.getElementById('myTabContent');
-    
+
     Object.keys(fleets).forEach(fleetNum => {
         let idHeader = `fleet${fleetNum}`;
         /*
@@ -134,7 +134,7 @@ let loadTable = () => {
         */
         let tabLI = document.createElement('LI');
         tabLI.setAttribute('class', 'nav-item');
-    
+
         let tabA = document.createElement('A');
         let tabId = `${idHeader}Tab`
         tabA.setAttribute('class', 'nav-link')
@@ -145,10 +145,10 @@ let loadTable = () => {
         tabA.setAttribute('aria-controls', idHeader);
         tabA.setAttribute('aria-selected', 'false');
         tabA.innerHTML = `Fleet ${fleetNum}`;
-    
+
         tabLI.appendChild(tabA);
         myTab.insertBefore(tabLI, myTab.children[1]);
-    
+
         /* 
             Now the tab content
         */
@@ -157,54 +157,54 @@ let loadTable = () => {
         tabContentContainer.setAttribute('id', idHeader);
         tabContentContainer.setAttribute('role', 'tabpanel');
         tabContentContainer.setAttribute('aria-labelledby', `${idHeader}Tab`)
-    
+
         let radioForm = document.createElement('FORM');
-    
+
         let row = document.createElement('DIV');
         row.setAttribute('class', 'row justify-content-center');
-    
+
         ['Map', 'Table'].forEach(e => {
             radDiv = document.createElement('DIV');
             e == 'Map' ?
                 radDiv.setAttribute('style', 'margin-right: 5vw;') :
                 radDiv.setAttribute('style', 'margin-left: 5vw;');
-    
+
             let radio = document.createElement('INPUT');
             radio.setAttribute('type', 'radio');
             radio.setAttribute('name', 'vis');
             radio.setAttribute('id', `${idHeader}${e}Rad`)
             if (e == 'Map') radio.setAttribute('checked', 'true');
-    
+
             let label = document.createElement('LABEL');
             label.setAttribute('for', `#${idHeader}${e}Rad`);
             label.innerHTML = e;
-    
+
             radDiv.appendChild(radio);
             radDiv.appendChild(label);
             row.appendChild(radDiv);
         })
-    
+
         radioForm.appendChild(row);
         tabContentContainer.appendChild(radioForm);
-    
+
         let mapDiv = document.createElement('DIV');
         mapDiv.setAttribute('id', `${idHeader}MapDiv`);
         mapDiv.setAttribute('class', 'mapDiv');
         mapDiv.innerHTML = 'Map goes here';
-    
+
         tabContentContainer.appendChild(mapDiv);
-    
+
         let tableDiv = document.createElement('DIV');
         tableDiv.setAttribute('id', `${idHeader}TableDiv`);
         tableDiv.setAttribute('class', 'tableDiv');
         fleetData = fleets[fleetNum];
         // console.log(fleetData);
-    
+
         let table = document.createElement('TABLE');
         table.setAttribute('id', `${idHeader}Table`);
         table.setAttribute('class', 'index table-bordered table-sm');
         table.setAttribute('max-width', '100vw');
-    
+
         let header = table.createTHead();
         header.insertRow(0);
         colNames.forEach(function (name) {
@@ -215,7 +215,7 @@ let loadTable = () => {
         });
         let tbody = document.createElement('TBODY');
         table.appendChild(tbody);
-    
+
         fleetData.forEach(entry => {
             let row = document.createElement('TR');
             entry.forEach((colVal, col, _) => {
@@ -231,7 +231,7 @@ let loadTable = () => {
             });
             tbody.appendChild(row);
         });
-    
+
         tableDiv.appendChild(table);
         tabContentContainer.appendChild(tableDiv);
         myTabContent.insertBefore(tabContentContainer, myTabContent.children[1]);
