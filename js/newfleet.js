d3a206eb6c7aa4e70600fe56a862f81c4461514f
Jeffrey Ng <komoto415@gmail.com>
2020-03-28 03:21:20 -0500

    streamlining code layout with similar methods

--- js/newfleet.js
+++ js/newfleet.js
@@ -1,7 +1,8 @@
 $('#newFleetForm').submit(async e => {
     e.preventDefault();
+
+    // console.log(e);
     
-    var url = 'https://supply.team22.softwareengineeringii.com/addFleet';
     var username = document.getElementById('accountName').text;
     console.log(username);
     var fleet = {'username': username};
@@ -9,13 +10,14 @@ $('#newFleetForm').submit(async e => {
     fields = form.querySelectorAll('input, select');
     // console.log(form);
     console.log(fields);
-
+    
     fields.forEach((e, i) => {
         // console.log(e.id);
         fleet[`${e.id}`] = e.value;
     });
     console.log(fleet);
-
+    
+    var url = 'https://supply.team22.softwareengineeringii.com/addFleet';
     await fetch(url, {
         method: 'POST',
         headers: {
