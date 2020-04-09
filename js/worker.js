aa0375dc189adf61bf8e27e3414591e01ba81379
Jeffrey Ng <komoto415@gmail.com>
2020-04-09 17:00:48 -0500

    fetch now inside a switch to be able to kill off the worker

--- js/worker.js
+++ js/worker.js
@@ -1,28 +1,35 @@
-self.addEventListener('message', function(e) {
+self.addEventListener('message', function (e) {
     console.log(e)
     var data = e.data;
-    var url = new URL("https://supply.team22.softwareengineeringii.com/getDispatch/"),
-        params = {
-            'vid': 28
-        }
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    // console.log(url);
-    var interval = setInterval(function() {
-        // console.log(data.msg);
-        fetch(url).then(function (resposne) {
-            // console.log(data);
-            resposne.json().then(function (jsonRes) {
-                if (resposne.status == 200) {
-                    // console.log('nice')
-                    self.postMessage(jsonRes);
-                } else {
-                    // console.log('not nice')
-                    self.postMessage('bad status')
-                }   
-            });
-        }).catch(function (error) {
-            console.log(error)
-            self.postMessage('fetch failed')
-        });
-    }, 5_000)
+    switch (data.cmd) {
+        case 'start':
+            var vid = data.vid;
+            var url = new URL("https://supply.team22.softwareengineeringii.com/getDispatch/"),
+                params = {
+                    'vid': vid
+                }
+            Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+            // console.log(url);
+            setInterval(function () {
+                // console.log(data.msg);
+                fetch(url).then(function (resposne) {
+                    // console.log(data);
+                    resposne.json().then(function (jsonRes) {
+                        if (resposne.status == 200) {
+                            // console.log('nice')
+                            self.postMessage(jsonRes);
+                        } else {
+                            // console.log('not nice')
+                            self.postMessage('bad status')
+                        }
+                    });
+                }).catch(function (error) {
+                    console.log(error)
+                    self.postMessage('fetch failed')
+                });
+            }, 5_000)
+        case 'stop':
+            self.close();
+            break;
+    };
 }, false);
