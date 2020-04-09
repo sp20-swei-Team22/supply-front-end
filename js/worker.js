97161bc2ccf07f43cd808599f2bf20b2317f9f84
Jeffrey Ng <komoto415@gmail.com>
2020-04-09 09:28:23 -0500

    added webworker to just fetch dispatch

---
+++ js/worker.js
@@ -0,0 +1,27 @@
+self.addEventListener('message', function(e) {
+    console.log(e)
+    var data = e.data;
+    var url = new URL("https://supply.team22.softwareengineeringii.com/getDispatch/"),
+    // params = {
+    //     'vid': 123
+    // }
+    var interval = setInterval(function() {
+        console.log(data.msg);
+        // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+        fetch(url).then(function (resposne) {
+            console.log(data);
+            resposne.json().then(function (jsonRes) {
+                if (resposne.status == 200) {
+                    console.log('nice')
+                    self.postMessage(jsonRes);
+                } else {
+                    console.log('not nice')
+                    self.postMessage('bad status')
+                }   
+            });
+        }).catch(function (error) {
+            console.log(error)
+            self.postMessage('fetch failed')
+        });
+    }, 5_000)
+}, false);
