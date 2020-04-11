1dee6cf926906ebaca82892f04e03d634ea542a4
Jeffrey Ng <komoto415@gmail.com>
2020-04-11 11:49:26 -0500

    giving workers another go. adjusted how fid is fetch due to div nesting of the toggles

--- js/togglelivefleetupdate.js
+++ js/togglelivefleetupdate.js
@@ -1,44 +1,62 @@
 var switchDict = {}
 $(document).on('change', '.updateSwitch', function (e) {
-    // console.log(e);
+    // console.log(e); 
     // console.log(this);
     // console.log(this.parentNode.parentNode)
+    // console.log(this.parentNode.parentNode.parentNode.parentNode.parentNode)
     // let switchInput = this.parentNode.parentNode.children[0].children[0];
     // let switchLabel = this.parentNode.parentNode.children[1];
-    let fid = this.parentNode.parentNode.parentNode.id
+    let fid = this.parentNode.parentNode.parentNode.parentNode.parentNode.id
     // console.log(fid);
     fid = fid.substring(fid.indexOf('t') + 1);
-    // console.log(fid);
+    console.log(fid);
+
+
+    if (fid in switchDict) {
+        switchDict[fid].postMessage({'cmd': 'stop'})
+        delete switchDict[fid]
+    } else {
+        var worker = new Worker('/supply-front-end/js/vehiclesworker.js');
+        worker.postMessage({ 'cmd': 'start', 'fid': fid });
+        worker.addEventListener('message', function (e) {
+            console.log(e.data);
+            // let dispatchJSON = e.data;
+            // let dispatchArr = formatDispatchJSON(dispatchJSON);
+        }, false);
+        switchDict[fid] = worker
+    }
 
     // console.log(switchInput);
     // console.log(switchLabel);
-    var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
-        params = {
-            'fid': fid
-        }
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    // console.log(url);
-    // console.log(switchDict);
-    if (fid in switchDict) {
-        clearInterval(switchDict[fid]);
-        delete switchDict[fid]
-        // console.log('turnning off')
-    } else {
-        var ping = setInterval(function () {
-            console.log(fid);
-            fetch(url).then(function (resposne) {
-                resposne.json().then(function (parsedJSON) {
-                    if (resposne.status == 200) {
-                        console.log(parsedJSON);
-                    } else {
-                        alert('something went wrong');
-                    }
-                });
-            }).catch(function (error) {
-                console.log(error)
-            });
-        }, 5_000);
-        switchDict[fid] = ping
-    }
+
+    // Below WORKS
+    // var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
+    //     params = {
+    //         'fid': fid
+    //     }
+    // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    // // console.log(url);
+    // // console.log(switchDict);
+    // if (fid in switchDict) {
+    //     clearInterval(switchDict[fid]);
+    //     delete switchDict[fid]
+    //     // console.log('turnning off')
+    // } else {
+    //     var ping = setInterval(function () {
+    //         console.log(fid);
+    //         fetch(url).then(function (resposne) {
+    //             resposne.json().then(function (parsedJSON) {
+    //                 if (resposne.status == 200) {
+    //                     console.log(parsedJSON);
+    //                 } else {
+    //                     alert('something went wrong');
+    //                 }
+    //             });
+    //         }).catch(function (error) {
+    //             console.log(error)
+    //         });
+    //     }, 5_000);
+    //     switchDict[fid] = ping
+    // }
     // console.log(switchDict);
 })
---
+++ js/vehiclesworker.js
@@ -0,0 +1,36 @@
+self.addEventListener('message', function (e) {
+    // console.log(e)
+    var data = e.data;
+    if (data.cmd == 'start') {
+        var fid = data.fid;
+        var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
+            params = {
+                'fid': fid
+            }
+        if (fid == 'home') {
+            url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/")
+        }
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+        // console.log(url);
+        setInterval(function () {
+            // console.log(data.msg);
+            fetch(url).then(function (resposne) {
+                // console.log(data);
+                resposne.json().then(function (jsonRes) {
+                    if (resposne.status == 200) {
+                        // console.log('nice')
+                        self.postMessage(jsonRes);
+                    } else {
+                        // console.log('not nice')
+                        self.postMessage('bad status')
+                    }
+                });
+            }).catch(function (error) {
+                console.log(error)
+                self.postMessage('fetch failed')
+            });
+        }, 5_000)
+    } else if (data.cmd == 'stop') {
+        self.close();
+    }
+}, false);
