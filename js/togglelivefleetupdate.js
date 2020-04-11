1ae7677e3e113628cf6cb6f34fc520dc17c45628
Jeffrey Ng <komoto415@gmail.com>
2020-04-11 12:19:35 -0500

    added tbody rebuilding for updated vehicle jsons

--- js/togglelivefleetupdate.js
+++ js/togglelivefleetupdate.js
@@ -19,44 +19,68 @@ $(document).on('change', '.updateSwitch', function (e) {
         var worker = new Worker('/supply-front-end/js/vehiclesworker.js');
         worker.postMessage({ 'cmd': 'start', 'fid': fid });
         worker.addEventListener('message', function (e) {
-            console.log(e.data);
-            // let dispatchJSON = e.data;
-            // let dispatchArr = formatDispatchJSON(dispatchJSON);
+            // console.log(e.data);
+            const generic = fid == 'home'
+            let vehiclesJSON = e.data;
+            let vehiclesData = formatVehicleJSON(vehiclesJSON, generic);
+            console.log(vehiclesData);
+            let vehicleTable = generic ? 
+                document.getElementById('homeTableTable') : 
+                document.getElementById(`fleet${fid}Table`);
+            // console.log(vehicleTable);
+            let oldTBody = vehicleTable.querySelectorAll('tbody')[0];
+            // console.log('Old ', oldTBody);
+            vehicleTable.removeChild(oldTBody);
+            let tbody = fillTBody(vehiclesData);
+            // console.log('New ', tbody);
+            vehicleTable.appendChild(tbody);
+
         }, false);
         switchDict[fid] = worker
     }
-
-    // console.log(switchInput);
-    // console.log(switchLabel);
-
-    // Below WORKS
-    // var url = new URL("https://supply.team22.softwareengineeringii.com/vehicleRequest/"),
-    //     params = {
-    //         'fid': fid
-    //     }
-    // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    // // console.log(url);
-    // // console.log(switchDict);
-    // if (fid in switchDict) {
-    //     clearInterval(switchDict[fid]);
-    //     delete switchDict[fid]
-    //     // console.log('turnning off')
-    // } else {
-    //     var ping = setInterval(function () {
-    //         console.log(fid);
-    //         fetch(url).then(function (resposne) {
-    //             resposne.json().then(function (parsedJSON) {
-    //                 if (resposne.status == 200) {
-    //                     console.log(parsedJSON);
-    //                 } else {
-    //                     alert('something went wrong');
-    //                 }
-    //             });
-    //         }).catch(function (error) {
-    //             console.log(error)
-    //         });
-    //     }, 5_000);
-    //     switchDict[fid] = ping
-    // }
     // console.log(switchDict);
 })
+
+
+function fillTBody(data) {
+    let tbody = document.createElement('TBODY');
+    table.appendChild(tbody);
+
+    data.forEach(entry => {
+        let row = document.createElement('TR');
+        entry.forEach((colVal, col, _) => {
+            let cell = document.createElement('TD');
+            cell.appendChild(document.createTextNode(colVal));
+            if (col == 0) {
+                row.setAttribute('id', `${idHeader}VID${colVal}`)
+                cell.setAttribute('onclick', 'getDispatch(this)');
+                cell.setAttribute('data-toggle', 'modal');
+                cell.setAttribute('data-target', '#dispatchRecordPopup')
+            }
+            row.append(cell);
+        });
+        tbody.appendChild(row);
+    });
+    return tbody;
+}
+
+function formatVehicleJSON(json, generic) {
+    arr = []
+    json.shift()
+    json.forEach(vehicleDict => {
+        // console.log(vehicleDict);
+        let dateAdded = vehicleDict['date_added']
+        let trimmedDate = dateAdded.substring(0, dateAdded.indexOf('T'));
+        arr.push(
+            [vehicleDict['fleetid'], vehicleDict['vehicleid'], vehicleDict['status'],
+            `${vehicleDict['make']}: ${vehicleDict['model']}`, trimmedDate,
+            vehicleDict['licenseplate'], vehicleDict['last_heartbeat']]
+        );
+    })
+    if (!generic) {
+        arr.forEach(entry => {
+            entry.shift()
+        })
+    }
+    return arr;
+}
