c6604677f5f577f9edaa71ac4b240ff5b7947c90
Jeffrey Ng <komoto415@gmail.com>
2020-04-13 20:01:50 -0500

    moving vehicle json formatting to independant script

---
+++ js/formatvehiclejson.js
@@ -0,0 +1,15 @@
+function formatVehicleJSON(json) {
+    arr = []
+    json.shift()
+    json.forEach(vehicleDict => {
+        // console.log(vehicleDict);
+        let dateAdded = vehicleDict['date_added']
+        let trimmedDate = dateAdded.substring(0, dateAdded.indexOf('T'));
+        arr.push(
+            [vehicleDict['vehicleid'], vehicleDict['fleetid'], vehicleDict['status'],
+            `${vehicleDict['make']}: ${vehicleDict['model']}`, trimmedDate,
+            vehicleDict['licenseplate'], vehicleDict['last_heartbeat']]
+        );
+    })
+    return arr;
+}
