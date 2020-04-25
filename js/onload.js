2598bd61d6f30f558df74861564de3ab6754df4d
Jeffrey Ng <komoto415@gmail.com>
2020-04-25 04:41:04 -0500

    now utilises buildTab method

--- js/onload.js
+++ js/onload.js
@@ -49,22 +49,23 @@ let loadTables = () => {
                                 /*
                                     First generate the tab
                                 */
-                                let tabLI = document.createElement('LI');
-                                tabLI.setAttribute('class', 'nav-item mytab');
+                               buildTab(idHeader)
+                                // let tabLI = document.createElement('LI');
+                                // tabLI.setAttribute('class', 'nav-item mytab');
 
-                                let tabA = document.createElement('A');
-                                let tabId = `${idHeader}Tab`
-                                tabA.setAttribute('class', 'nav-link')
-                                tabA.setAttribute('id', tabId);
-                                tabA.setAttribute('data-toggle', 'tab');
-                                tabA.setAttribute('href', `#${idHeader}`);
-                                tabA.setAttribute('rold', 'tab');
-                                tabA.setAttribute('aria-controls', idHeader);
-                                tabA.setAttribute('aria-selected', 'false');
-                                tabA.innerHTML = `Fleet ${fleetNum}`;
+                                // let tabA = document.createElement('A');
+                                // let tabId = `${idHeader}Tab`
+                                // tabA.setAttribute('class', 'nav-link')
+                                // tabA.setAttribute('id', tabId);
+                                // tabA.setAttribute('data-toggle', 'tab');
+                                // tabA.setAttribute('href', `#${idHeader}`);
+                                // tabA.setAttribute('rold', 'tab');
+                                // tabA.setAttribute('aria-controls', idHeader);
+                                // tabA.setAttribute('aria-selected', 'false');
+                                // tabA.innerHTML = `Fleet ${fleetNum}`;
 
-                                tabLI.appendChild(tabA);
-                                myTab.insertBefore(tabLI, myTab.children[1]);
+                                // tabLI.appendChild(tabA);
+                                // myTab.insertBefore(tabLI, myTab.children[1]);
 
                             })
                             mapboxgl.accessToken = 'pk.eyJ1Ijoia29tb3RvNDE1IiwiYSI6ImNrOHV1cGp3bDA1bG0zZ282bmZhdDZjeWYifQ.2w_4X8WR5lFXvsmp6TeHEg';
